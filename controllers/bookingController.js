01dd69497873b924f29de9889fc5a45647641bb4
janisgee <janisgeegee@gmail.com>
2023-10-17 15:21:25 +0800

    Correct the price variable in createBookingCheckout function

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,7 +65,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async function(session){
   const tour = session.client_reference_id;
   const user = await User.findOne({email:session.customer_email}).id;
-  const price = session.amount_total /100;
+  const price = session.line_items[0].price_data.unit_amount/100;
 
   await Booking.create({
     tour,
